c0ecfcf72f797fa7bee866bf3c740a21f191763a
Sparsh0508 <157520563+Sparsh0508@users.noreply.github.com>
2025-12-30 18:06:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        int ans = 0;
-        for(int i = 0;i<nums.length;i++){
-            ans = Math.max(ans,help(nums,dp,i));
+        int[][] dp = new int[nums.length+1][nums.length+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
         }
-        return ans;
+        return help(nums,0,-1,dp);
     }
-    public static int help(int[] nums,int[] dp,int i){
-        if(dp[i] != -1){
-            return dp[i];
+    public static int help(int[] nums,int curr,int prev,int[][] dp){
+        if(curr == nums.length){
+            return 0;
         }
-        int maxLen = 1;
-        for(int j = i+1;j<nums.length;j++){
-            if(nums[j]>nums[i]){
-                maxLen = Math.max(maxLen,1+help(nums,dp,j));
-            }
+        if(dp[curr][prev+1] != -1){
+            return dp[curr][prev];
         }
-        return dp[i] = maxLen;
+        int take = 0;
+        if(prev == -1 || nums[curr] > nums[prev]){
+            take = 1 + help(nums,curr+1,curr,dp);
+        }
+
+        int notTake = 0 + help(nums,curr+1,prev,dp);
+        return dp[curr][prev+1] = Math.max(take,notTake);
     }
 }
