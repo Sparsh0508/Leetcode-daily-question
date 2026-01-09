0fc720f002b17af82832e71628eddb12daec1a00
Sparsh0508 <157520563+Sparsh0508@users.noreply.github.com>
2026-01-10 01:45:41 +0530

    Time: 33 ms (9.85%), Space: 54.8 MB (59.28%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,12 +1,23 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[]dp = new int[amount+1];
-        dp[0] = 1;
-        for(int coin : coins){
-            for(int i= coin;i<=amount;i++){
-                dp[i] += dp[i-coin]; 
-            }
+        Integer[][] memo = new Integer[coins.length][amount+1];
+        return help(coins,amount,memo,0);
+    }
+    public static int help(int[] coins,int sum,Integer[][] memo,int i){
+        if(sum == 0){
+            return 1;
         }
-        return dp[amount];
+        if(coins.length == i){
+            return 0;
+        }
+        if(memo[i][sum] != null){
+            return memo[i][sum];
+        }
+        int take = 0;
+        if(coins[i] <= sum){
+            take = help(coins,sum-coins[i],memo,i);
+        }
+        int skip = help(coins,sum,memo,i+1);
+        return memo[i][sum] = take+skip;
     }
 }
