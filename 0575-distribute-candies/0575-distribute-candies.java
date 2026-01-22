dc899615166be7c749e4b34b7e8954695aac8784
Sparsh0508 <157520563+Sparsh0508@users.noreply.github.com>
2026-01-22 12:09:20 +0530

    Time: 30 ms (92.44%), Space: 48.7 MB (39.78%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -2,9 +2,10 @@ class Solution {
     public int distributeCandies(int[] candy) {
         int val = candy.length/2;
         HashSet<Integer> hs = new HashSet<>();
-        for(int num : candy){
-            hs.add(num);
+        for(int i = 0;i<candy.length ;i++){
+            hs.add(candy[i]);
         }
+
         return Math.min(hs.size(),val);
     }
 }
