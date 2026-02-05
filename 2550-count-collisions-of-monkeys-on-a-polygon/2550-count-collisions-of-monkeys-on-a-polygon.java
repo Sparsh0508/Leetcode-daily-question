0d4249ecb5e2875a520ed22d267ad8cdce40ebc4
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-02-06 00:32:46 +0530

    Time: 0 ms (100%), Space: 42.4 MB (6.71%) - LeetHub

--- 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.java
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.java
@@ -1,13 +1,20 @@
 class Solution {
+    public long mod = 1_000_000_007;
     public int monkeyMove(int n) {
-        long res = 1;
-        long base = 2;
-        int i = 0;
-        while(i<n){
-            res = (2*res) % 1_000_000_007;
-            i++;
+        long res = help(2,n);
+        return (int)((res - 2 + mod) % mod);
+    }
+    public long help(int base,int exp){
+        if(exp == 0){
+            return 1;
         }
-        return (int)((res - 2 + 1_000_000_007) % 1_000_000_007);
 
+        long half = help(2,exp/2);
+        long res = (half*half) % mod;
+
+        if ((exp & 1) == 1) {
+            res = (res * base) % mod;
+        }
+        return res;
     }
 }
