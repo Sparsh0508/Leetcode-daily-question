7e16cd3023ffd6232672107b128e917678a72873
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-02-15 12:06:28 +0530

    Time: 278 ms (17.06%), Space: 161.6 MB (5.04%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,6 +1,8 @@
 class Solution {
+    Boolean[][] dp ;
     public String longestPalindrome(String s) {
         int n = s.length();
+        dp = new Boolean[n+1][n+1];
         int max = Integer.MIN_VALUE;
         int st = 0;
         for(int i = 0;i<s.length();i++){
@@ -19,9 +21,13 @@ class Solution {
         if(i >=j){
             return true;
         }
-        if(s.charAt(i) == s.charAt(j)){
-            return check(s,i+1,j-1);
+        if(dp[i][j] != null){
+            return dp[i][j];
         }
-        return false;
+        boolean res = false;
+        if(s.charAt(i) == s.charAt(j)){
+            res= check(s,i+1,j-1);
+        }
+        return dp[i][j] = res;
     }
 }
