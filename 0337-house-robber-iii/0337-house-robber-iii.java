1f6e242f398bf66ce8e5c6477864569adcc960c1
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-02-20 13:03:26 +0530

    Time: 0 ms (100%), Space: 46.9 MB (19.94%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int rob(TreeNode root) {
+        int[] res = help(root);
+        return Math.max(res[0], res[1]);
+    }
+    private int[] help(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = help(root.left);
+        int[] right = help(root.right);
+
+        int robbed = root.val + left[0] + right[0];
+        int notRobbed = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return new int[]{notRobbed, robbed};
+    }
+}
