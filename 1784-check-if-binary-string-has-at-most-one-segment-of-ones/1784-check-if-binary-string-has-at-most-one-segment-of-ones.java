2c6fcfa4d711faf3c83aaaf3eab30b572cf6b61e
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-03-06 21:39:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        
+        if(s.length() == 1){
+            return true;
+        }
+        boolean zeroPass = false;
+        for(int i = 1;i<s.length();i++){
+            if(s.charAt(i) == '0'){
+                zeroPass = true;
+            }else{
+                if(zeroPass){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
