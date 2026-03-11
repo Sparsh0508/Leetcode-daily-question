856da07e266ec4364c53e9ceba32056e52d179bd
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-03-11 10:53:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -12,20 +12,40 @@ class Solution {
             graph.get(v).add(u);
         }
         boolean[] vis = new boolean[n];
-        return dfs(graph,vis,n,source,destination);
+        // return dfs(graph,vis,n,source,destination);
+        return bfs(graph,vis,n,source,destination);
     }
-    public static boolean dfs(ArrayList<ArrayList<Integer>> graph,boolean[] vis ,int n,int src,int dest){
-        if(src == dest){
-            return true;
-        }
+    // public static boolean dfs(ArrayList<ArrayList<Integer>> graph,boolean[] vis ,int n,int src,int dest){
+    //     if(src == dest){
+    //         return true;
+    //     }
+    //     vis[src] = true;
+    //     for(int nei : graph.get(src)){
+    //         if(!vis[nei]){
+    //             if(dfs(graph,vis,n,nei,dest)){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    public static boolean bfs(ArrayList<ArrayList<Integer>> graph,boolean[] vis ,int n,int src,int dest){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
         vis[src] = true;
-        for(int nei : graph.get(src)){
-            if(!vis[nei]){
-                if(dfs(graph,vis,n,nei,dest)){
-                    return true;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            if(node == dest){
+                return true;
+            }
+            for(int nei : graph.get(node)){
+                if(!vis[nei]){
+                    q.add(nei);
+                    vis[nei] = true;
                 }
             }
         }
         return false;
     }
+
 }
