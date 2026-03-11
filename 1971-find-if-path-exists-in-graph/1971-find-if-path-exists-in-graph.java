2225be7c3001b032f161679079dd2ace29d9490a
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-03-11 10:47:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,50 +1,31 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int src, int dest) {
-        List<List<Integer>> graph = new ArrayList<>();
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         for(int i = 0;i<n;i++){
             graph.add(new ArrayList<>());
-        }    
-        for(int[] i : edges){
-            graph.get(i[0]).add(i[1]);
-            graph.get(i[1]).add(i[0]);
         }
-        // return bfs(graph,src,dest);
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
 
-        boolean[] vis = new boolean[graph.size()];
-        return dfs(graph,src,dest,vis);
-    }
-    public static boolean bfs(List<List<Integer>> graph,int src,int dest){
-        boolean[] visit = new boolean[graph.size()];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(src);
-        visit[src] = true;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            if(node == dest){
-                return true;
-            }
-            for(int i : graph.get(node)){
-                if(!visit[i]){
-                    q.add(i);
-                    visit[i] = true;
-                }
-            }
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
-        return false;
+        boolean[] vis = new boolean[n];
+        return dfs(graph,vis,n,source,destination);
     }
-
-    public static boolean dfs(List<List<Integer>> graph,int src,int dest,boolean[] vis){
+    public static boolean dfs(ArrayList<ArrayList<Integer>> graph,boolean[] vis ,int n,int src,int dest){
         if(src == dest){
             return true;
         }
         vis[src] = true;
         for(int nei : graph.get(src)){
             if(!vis[nei]){
-                if(dfs(graph,nei,dest,vis)){
+                if(dfs(graph,vis,n,nei,dest)){
                     return true;
                 }
             }
         }
         return false;
     }
-}   
+}
