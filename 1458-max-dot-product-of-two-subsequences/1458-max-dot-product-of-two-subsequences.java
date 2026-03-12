7165cf5e6aa05e58d3c2daa8a03f2bde39d4b44b
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-03-12 19:22:14 +0530

    Time: 11 ms (48.67%), Space: 48.2 MB (14.23%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,25 +1,21 @@
 class Solution {
+    Integer[][] dp;
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length+1][nums2.length+1];
-        for(int[] arr : dp){
-            Arrays.fill(arr,-1);
-        }
-        return help(nums1, nums2, 0, 0, dp);
+        dp = new Integer[nums1.length][nums2.length]; 
+        return help(nums1,nums2,0,0);   
     }
-
-    public int help(int[] nums1, int[] nums2, int i, int j,int[][] dp) {
-        if (i == nums1.length || j == nums2.length) {
+    public int help(int[] nums1,int[] nums2,int i,int j){
+        if(nums1.length == i || nums2.length == j){
             return Integer.MIN_VALUE;
         }
-        if(dp[i][j] != -1){
+        if(dp[i][j] != null){
             return dp[i][j];
         }
+        int take = (nums1[i]*nums2[j]) + Math.max(0,help(nums1,nums2,i+1,j+1));
 
-        int take = nums1[i] * nums2[j] + Math.max(0, help(nums1, nums2, i + 1, j + 1,dp));
+        int skip1 = help(nums1,nums2,i+1,j);
+        int skip2 = help(nums1,nums2,i,j+1);
+        return dp[i][j] =  Math.max(take,Math.max(skip1,skip2));
 
-        int skip1 = help(nums1, nums2, i + 1, j, dp);
-        int skip2 = help(nums1, nums2, i, j + 1, dp);
-
-        return dp[i][j] = Math.max(take, Math.max(skip1, skip2));
     }
 }
