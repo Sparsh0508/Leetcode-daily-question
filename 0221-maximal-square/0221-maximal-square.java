98b467441d9cf6a2999641e7bbcff0f44216201e
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-03-16 14:35:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,30 +1,34 @@
 class Solution {
+    int max = 0;
+    Integer[][] dp;
     public int maximalSquare(char[][] mat) {
-        int[] maxa = new int[1];
-        int[][] dp = new int[mat.length][mat[0].length];
-        for(int[] arr : dp){
-            Arrays.fill(arr,-1);
+        int m = mat.length;
+        int n = mat[0].length;
+        dp = new Integer[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                help(mat,i,j);
+            }
         }
-        help(mat,0,0,maxa,dp);
-        return maxa[0]*maxa[0];
+        return max * max;
     }
-    public static int help(char[][] mat,int i,int j,int[] maxa,int[][] dp) {
-        if(i  >= mat.length || j >= mat[0].length){
+
+    public int help(char[][] mat,int i,int j){
+        if(i >= mat.length || j >= mat[0].length){
             return 0;
         }
-        if(dp[i][j] != -1){
+        if(dp[i][j] != null){
             return dp[i][j];
         }
-        int right = help(mat,i,j+1,maxa,dp);
-        int diag = help(mat,i+1,j+1,maxa,dp);
-        int down = help(mat,i+1,j,maxa,dp);
-        
+        int right = help(mat,i,j+1);
+        int down = help(mat,i+1,j);
+        int diag = help(mat,i+1,j+1);
+
         if(mat[i][j] == '1'){
-            int ans = 1+ Math.min(right,Math.min(diag,down));
-            maxa[0] = Math.max(ans,maxa[0]);
-            return dp[i][j] = ans;
-        }else{
-            return dp[i][j] =  0;
+            int ans = 1 + Math.min(right,Math.min(down,diag));
+            max = Math.max(max,ans);
+            return dp[i][j] =ans;
         }
+        return dp[i][j] = 0;
     }
 }
