86b9809d1b9ca45f8b05a10f336881c150e04c6d
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-03-27 14:55:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
@@ -1,22 +1,13 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-    int[] line = new int[102];
-    int points_on_line = 0;
-
-    for (int i = 0; i < nums.size(); i++) {
-        int s = nums.get(i).get(0);
-        int e = nums.get(i).get(1);
-        line[s] += 1;
-        line[e + 1] -= 1;
-    }
-
-    for (int i = 1; i < 102; i++) {
-        line[i] += line[i - 1];
-        if (line[i] != 0) {
-            points_on_line += 1;
+      HashSet<Integer> hs = new HashSet<>();
+      for(int i = 0;i<nums.size();i++){
+        int st = nums.get(i).get(0);
+        int ed = nums.get(i).get(1);
+        for(int j = st;j<=ed;j++){
+            hs.add(j);
         }
-    }
-
-    return points_on_line;
+      }   
+      return hs.size();
     }
 }
