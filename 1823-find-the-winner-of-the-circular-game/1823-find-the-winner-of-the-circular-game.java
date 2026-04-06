c2cab5cc08ddd0e7c0b99b449d76b792c08b9df1
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-04-06 15:28:58 +0530

    Time: 45 ms (13.09%), Space: 46.3 MB (22.96%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -4,14 +4,16 @@ class Solution {
         for(int i = 1;i<= n;i++){
             q.add(i);
         }
+        int x = 1;
         while(q.size() != 1){
-            int x = 1;
-            while(x < k){
+            if(x<k){
                 q.add(q.remove());
                 x++;
             }
-            q.remove();
-            
+            else{
+                q.remove();
+                x = 1;
+            }
         }
         return q.peek();
     }
