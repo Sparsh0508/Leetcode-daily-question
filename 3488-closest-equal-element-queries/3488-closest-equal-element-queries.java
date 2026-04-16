8ff84c7fc92bc53a98efd0a12e1b8c72e06b2263
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-04-16 15:03:06 +0530

    Time: 104 ms (64.65%), Space: 187.3 MB (53.54%) - LeetHub

---
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            hm.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        for (int qi : queries) {
+            int idx = qi;
+            int ele = nums[idx];
+
+            List<Integer> arr = hm.get(ele);
+            int sz = arr.size();
+
+            if (sz == 1) {
+                res.add(-1);
+                continue;
+            }
+
+            int pos = Collections.binarySearch(arr, idx);
+
+            int ans = Integer.MAX_VALUE;
+            int right = arr.get((pos + 1) % sz);
+            int d = Math.abs(idx - right);
+            int circular = n - d;
+            ans = Math.min(ans, Math.min(d, circular));
+            int left = arr.get((pos - 1 + sz) % sz);
+            d = Math.abs(idx - left);
+            circular = n - d;
+            ans = Math.min(ans, Math.min(d, circular));
+
+            res.add(ans);
+        }
+
+        return res;
+    }
+}
