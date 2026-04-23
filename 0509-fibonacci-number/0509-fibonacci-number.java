76bad14b7da8f0d16cdaaf0fe49de1fa1bf976b1
Sparsh Agarwal <157520563+Sparsh0508@users.noreply.github.com>
2026-04-23 10:42:19 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.21%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return help(n,dp);
+    }
+    public static int help(int n,int[] dp){
+        if(n == 0||n==1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = help(n-2,dp) + help(n-1,dp);
+    }
+}
